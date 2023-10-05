4b946d0733d7a37ff036ccea3407f96a747dd6d4
mxsurft <ccunjoku@gmail.com>
2023-10-05 13:37:34 +0100

    added comments to the code  and removed strings from the MONGO_URI

--- app.js
+++ app.js
@@ -1,27 +1,39 @@
+// Load environment variables
 require('dotenv').config()
 
+// Import express library
 const express = require('express')
+
+// Create an instance of express
 const app = express()
 
+// Import connect function from db/connect.js file
 const connectDB = require('./db/connect')
 
+// Define the port numnber for the server
 const port = 3000
 
+// Send a welcome message to the client
 app.get('/', (req, res)=>{
     res.status(200).send('Welcome to our Server')
 })
 
+// Define an async function to connect to the database and start
 const start = async ()=>{
     try{
-        await connectDB(process.env.MONGO_URI)
+        // Connect to MongoDB database
+        await connectDB(process.env.MONGO_URI) 
+         
+        // Log a message to the console indicating that the server is up and running
         app.listen(port, ()=>{
-            console.log(`Server is up and running in ${port}`)
+            console.log(`Server is up and running in ${port}`)  
         })
     }catch(error){
-        console.log(error)
+        console.log(error)  // Log any errors that occur during the connection process
     }
 }
 
+// Call the start function to start the server
 start()
 
 
